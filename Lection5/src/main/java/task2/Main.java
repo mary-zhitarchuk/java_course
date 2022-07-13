c2022ab03625540f0fe891abfa15633d0bd31a5d
mary-zhitarchuk <bacyjitarchuk@mail.ru>
2022-07-13 20:14:34 +0400

    Lection5 task2

--- Lection5/src/main/java/task2/Main.java
+++ Lection5/src/main/java/task2/Main.java
@@ -5,8 +5,20 @@ package main.java.task2;
     Продемонстрировать работу;
 */
 
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Member;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Map<Integer, Integer> map = (Map<Integer, Integer>) Proxy.newProxyInstance(
+                ClassLoader.getSystemClassLoader(), new Class[]{Map.class},
+                new MyInvocationHandler(new HashMap<Integer,Integer>())
+        );
+        MyInvocationHandler myInvocationHandler = new MyInvocationHandler(new HashMap<Integer,Integer>());
+        map.put(1,1);
     }
 }
---
+++ Lection5/src/main/java/task2/MyInvocationHandler.java
@@ -0,0 +1,22 @@
+package main.java.task2;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+public class MyInvocationHandler implements InvocationHandler {
+    private final Object delegate;
+    public  MyInvocationHandler(Object delegate){
+        this.delegate = delegate;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        System.out.println("Метод: " + method.getName());
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Время выполнения " + (endTime - startTime) + " milliseconds");
+        return null;
+    }
+
+}
